d1a8f7f9a0fcd9f4993d702e21e7e362d32b713e
Blatantbeast <35450175+Blatantbeast@users.noreply.github.com>
2018-10-25 16:31:34 +0200

    Add files via upload

---
+++ ragequit-master/RageQuit/Assets/Interactions/Scripts/PickMoney.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PickMoney : MonoBehaviour {
+
+    public Text ScoreText;
+    private int Score;
+
+	// Use this for initialization
+	void Start ()
+    {
+        Score = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        SetScore();
+		
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("PickUp"))
+        {
+            other.gameObject.SetActive(false);
+            Score += 1;
+            
+
+        }
+    }
+
+    void SetScore()
+    {
+        ScoreText.text = "Score: " + Score;
+    }
+}
---
+++ ragequit-master/RageQuit/Assets/Menus/MainMenu.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour {
+
+	public void LoadScene(string scene)
+    {
+        SceneManager.LoadScene(scene);
+        //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
